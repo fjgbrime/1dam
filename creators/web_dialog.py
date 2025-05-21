e8fa2c6ed03193a2db1de81b995dcc6559d90c0d
fjgbrime <56471564+fjgbrime@users.noreply.github.com>
2025-05-21 17:54:27 +0200

    Delete creators directory

--- creators/web_dialog.py
+++
@@ -1,7 +0,0 @@
-from .dialog import Dialog
-from products.button import Button # Adjusted import path
-from products.html_button import HTMLButton # Adjusted import path
-
-class WebDialog(Dialog):
-    def create_button(self) -> Button:
-        return HTMLButton()
